357077450ba7e3b4e311f2d68bd7472d351274fa
Walsker <wal.gatlei@gmail.com>
2018-12-27 18:31:35 -0500

    MMaking platform specific buttons part 2

--- src/aboutScreen/aboutPage.js
+++ src/aboutScreen/aboutPage.js
@@ -28,32 +28,31 @@ export default class AboutPage extends Component
 					<View style = {containerStyle.form}>
 						<View style = {containerStyle.formSection}>
 							<Text style = {textStyle.bold(70, 'center')}>Grade Aid</Text>
-							<Text style = {textStyle.regular(16, 'center', colors.secondaryTextColor)}>v1.1</Text>
+							<Text style = {textStyle.regular(15, 'center', colors.secondaryTextColor)}>v1.1</Text>
 						</View>
 						<View style = {containerStyle.formSection}>
-							<Text style = {textStyle.regular(20, 'center')}>Created by Wal Wal</Text>
+							<Text style = {textStyle.regular(21, 'center')}>Created by Wal Wal</Text>
 						</View>
 						<Divider color = {colors.dividerColor} separation = {10} style = {{marginTop: 0}}/>
 						<View style = {containerStyle.formSection}>
-							<Text style = {textStyle.light(20, 'center')}>Found a bug? Feedback? Feature Requests?</Text>
 							<View style = {{marginVertical: 5}}/>
 							<View style = {containerStyle.roundedBox}>
-								<Text style = {textStyle.regular(24, 'center')}>Contact me at</Text>
+								<Text style = {textStyle.regular(18, 'center')}>Contact me</Text>
 								<TouchableOpacity
 									onPress = {() => Linking.openURL('mailto:wal.gatlei@gmail.com?subject=[Grade Aid]').catch(err => console.log('Could not open mail app.', err))}
 								>
 									<View style = {{marginVertical: 10}}>
-										<Text style = {textStyle.regular(30, 'center', colors.primaryColor)}>wal.gatlei@gmail.com</Text>
+										<Text style = {textStyle.regular(21, 'center', colors.primaryColor)}>wal.gatlei@gmail.com</Text>
 									</View>
 								</TouchableOpacity>
 							</View>
 							<View style = {containerStyle.roundedBox}>
-								<Text style = {textStyle.regular(24, 'center')}>Github</Text>
+								<Text style = {textStyle.regular(18, 'center')}>Github</Text>
 								<TouchableOpacity
 									onPress = {() => Linking.openURL('https://github.com/walsker').catch(err => console.log('Could not open browser.', err))}
 								>
 									<View style = {{marginVertical: 10}}>
-										<Text style = {textStyle.regular(30, 'center', colors.primaryColor)}>github.com/walsker</Text>
+										<Text style = {textStyle.regular(21, 'center', colors.primaryColor)}>github.com/walsker</Text>
 									</View>
 								</TouchableOpacity>
 							</View>
